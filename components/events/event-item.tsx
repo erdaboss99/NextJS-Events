99dde9f794c9cdb78ec17a199e663c7bad1f447c
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-02-24 20:10:28 +0100

    Implement the button and the icon components

--- components/events/event-item.tsx
+++ components/events/event-item.tsx
@@ -1,6 +1,8 @@
-import Link from 'next/link';
-
 import { EventData } from '@/dummy-data';
+import Button from '../ui/button';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+import DateIcon from '../icons/date-icon';
 
 const EventItem = ({ id, title, description, location, date, image, isFeatured }: EventData) => {
 	const formattedDate: string = new Date(date).toLocaleDateString('hu-HU', {
@@ -24,18 +26,23 @@ const EventItem = ({ id, title, description, location, date, image, isFeatured }
 				<div>
 					<h2 className='mx-0 my-[0.5rem] md:mx-0 md:my-[1rem]'>{title}</h2>
 					<div className='flex gap-[0.5rem] items-center'>
+						<DateIcon />
 						<time className='text-gray-500 font-bold'>{formattedDate}</time>
 					</div>
 					<div className='flex gap-[0.5rem] items-center'>
+						<AddressIcon />
 						<address className='text-gray-500 mx-0 my-[0.5rem] whitespace-pre'>
 							{formattedAddress}
 						</address>
 					</div>
 				</div>
 				<div className='flex flex-col p-[1rem] md:flex-row md:justify-end'>
-					<Link className='block' href={exploreLink}>
-						Explore Event
-					</Link>
+					<Button link={exploreLink}>
+						<span className='align-middle'>Explore Event</span>
+						<span className='align-middle ml-[0.5rem] inline-flex justify-center items-center'>
+							<ArrowRightIcon />
+						</span>
+					</Button>
 				</div>
 			</div>
 		</li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
 		extend: {
 			boxShadow: {
 				c1: '0 1px 4px rgba(0, 0, 0, 0.3), 0 1px 12px 2px rgba(0, 0, 0, 0.2)',
+				c2: '0 1px 6px rgba(0, 0, 0, 0.2)',
 			},
 		},
 	},
