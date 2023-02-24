1a5fefbfcdbb02837d80a1d53f6cb3ee46c4e265
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-02-24 20:09:45 +0100

    Add custom button component

---
+++ components/ui/button.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+interface ButtonProps {
+	link: string;
+	children: React.ReactNode;
+}
+
+const Button = (props: ButtonProps) => {
+	return (
+		<Link
+			href={props.link}
+			className='block align-middle no-underline cursor-pointer bg-cyan-500 border-solid border-[1px] border-cyan-500 rounded-[6px] text-cyan-100 px-[1.5rem] py-[0.5] text-center shadow-c2 hover:bg-cyan-700 hover:border-cyan-700 active:bg-cyan-700 active:border-cyan-700'>
+			{props.children}
+		</Link>
+	);
+};
+
+export default Button;
