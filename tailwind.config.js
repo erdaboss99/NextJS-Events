6fe92a281b16f92468d9bbb5ff7496203fbe5a80
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-02-25 22:46:11 +0100

    Add custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
 			boxShadow: {
 				c1: '0 1px 4px rgba(0, 0, 0, 0.3), 0 1px 12px 2px rgba(0, 0, 0, 0.2)',
 				c2: '0 1px 6px rgba(0, 0, 0, 0.2)',
+				c3: '0 2px 6px rgba(0, 0, 0, 0.2)',
+			},
+			backgroundImage: {
+				c1: 'linear-gradient(to bottom left,  #008b79, #1180a1)',
 			},
 		},
 	},
