83a0761b931267ac1bba71f46a8b8cf3fea2cbaa
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2023-02-27 12:05:40 +0100

    Reformat Button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,17 +1,25 @@
 import Link from 'next/link';
 
 interface ButtonProps {
-	link: string;
+	link?: string;
 	children: React.ReactNode;
 }
 
 const Button = (props: ButtonProps) => {
+	if (props.link) {
+		return (
+			<Link
+				href={props.link}
+				className='block align-middle no-underline cursor-pointer bg-cyan-500 border-solid border-[1px] border-cyan-500 rounded-[6px] text-cyan-100 px-[1.5rem] py-[0.5] text-center shadow-c2 hover:bg-cyan-700 hover:border-cyan-700 active:bg-cyan-700 active:border-cyan-700'>
+				{props.children}
+			</Link>
+		);
+	}
+
 	return (
-		<Link
-			href={props.link}
-			className='block align-middle no-underline cursor-pointer bg-cyan-500 border-solid border-[1px] border-cyan-500 rounded-[6px] text-cyan-100 px-[1.5rem] py-[0.5] text-center shadow-c2 hover:bg-cyan-700 hover:border-cyan-700 active:bg-cyan-700 active:border-cyan-700'>
+		<button className='block align-middle no-underline cursor-pointer bg-cyan-500 border-solid border-[1px] border-cyan-500 rounded-[6px] text-cyan-100 px-[1.5rem] py-[0.5] text-center shadow-c2 hover:bg-cyan-700 hover:border-cyan-700 active:bg-cyan-700 active:border-cyan-700'>
 			{props.children}
-		</Link>
+		</button>
 	);
 };
 
